b651041b82f92214cdb9222827f238c41ff1e56f
unknown <mapulgarin2@gmail.com>
2023-09-25 22:24:44 -0500

    Se crea funcion buscar_artista

--- Unidad 3/buscar_cancion.py
+++ Unidad 3/buscar_cancion.py
@@ -1,6 +1,7 @@
 musica = [("Hollywood Babylon","Misfits",8.9),
           ("Cold feelings","Social Distortion",9.9),
-          ("Alien","Pennywisw",9.8),
+          ("Alien","Pennywise",9.8),
+          ("My own country","Pennywise",9.6),
           ("Poison Heart","Ramones",8.0)]
 
 print(type(musica))
@@ -11,7 +12,16 @@ def buscar(nombre):
             return cancion
     return None
 
-print(buscar("Poison Heart"))    
+def buscar_artista(artista):
+    lista_artista = []
+    for banda in musica:
+        if banda[1]==artista:
+            lista_artista.append(banda)
+    return lista_artista
+      
+ 
+print(buscar("Poison Heart"))   
+print(buscar_artista("Pennywise")) 
         
         
     
