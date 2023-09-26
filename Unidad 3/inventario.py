2e0b4642ffaf3ab11ba22021b92e1bfce644881e
unknown <mapulgarin2@gmail.com>
2023-09-26 13:05:37 -0500

    Se realiza todo el codigo en inventario.py

--- Unidad 3/inventario.py
+++ Unidad 3/inventario.py
@@ -1,6 +1,43 @@
 inventario = {"Manzana":10,
               "Mango": 4,
               "Banano": 7}
+print(inventario)
 
 def gestion_inventario(accion,articulo,cantidad = 0):
-    pass
+    if accion == "Agregar":
+        if articulo in inventario:
+            inventario[articulo] += cantidad
+        else :
+            inventario[articulo] = cantidad
+    elif   accion == "Quitar" :
+        if articulo in inventario and inventario[articulo] >= cantidad:
+            inventario[articulo]-=cantidad
+        else:
+            print("No se puede realizar la acción")    
+    elif accion == "Buscar" :
+        if articulo in inventario:
+            return inventario[articulo]
+        else:
+            return None
+    else:
+        print("Accion no reconocida")
+            
+    
+gestion_inventario("Agregar","Manzana",5)  
+print(inventario)  
+    
+gestion_inventario("Quitar","Mango",4)  
+print(inventario)  
+    
+ 
+print(gestion_inventario("Buscar","Banano") )      
+        
+        
+            
+            
+        
+           
+        
+        
+        
+    
