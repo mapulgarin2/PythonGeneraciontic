78109f9be52911f2c1282702c04504b8726b7b80
unknown <mapulgarin2@gmail.com>
2023-10-03 22:24:38 -0500

    Se modifica archivo ordenacion_burbuja.py con la creacion de la funcion linear_search()

--- Unidad 3/ordenacion_burbuja.py
+++ Unidad 3/ordenacion_burbuja.py
@@ -1,17 +1,36 @@
 
-def burble_sort(array):
-    largo = len(array) - 1
-    #Bucle para las pasadas
-    for i in range(0,largo):
-        #bucle para comparaciones e intercambios
-        for j in range(0,largo):
-            if array[j] > array[j + 1]:
-                aux = array[j]
-                array[j]= array[j+1]
-                array[j +1]= aux
-    return array        
+# def burble_sort(array):
+#     largo = len(array) - 1
+#     #Bucle para las pasadas
+#     for i in range(0,largo):
+#         #bucle para comparaciones e intercambios
+#         for j in range(0,largo):
+#             if array[j] > array[j + 1]:
+#                 aux = array[j]
+#                 array[j]= array[j+1]
+#                 array[j +1]= aux
+#     return array        
         
-array_1 = [2,45,7,8,56,3,2,4,7,29,56,9]    
-print(array_1)    
+# array_1 = [2,45,7,8,56,3,2,4,7,29,56,9]    
+# print(array_1)    
 
-print(burble_sort(array_1))
+# print(burble_sort(array_1))
+
+#________________busqueda lineal___________________________
+
+def linear_search(lista,elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i
+    return -1
+
+numeros = [2,5,7,9,45,1,90, 0]
+elemento_busacado = 3
+indice = linear_search(numeros,elemento_busacado)
+
+if indice != -1 :
+    print(f"El elemento busacado {elemento_busacado} se encuentra en la lista {numeros}")
+else :
+    print(f"El elemento busacado {elemento_busacado} no se encuentra en la lista {numeros}")    
+ 
+        
